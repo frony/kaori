c26056bc26e24823f4dfefff1c5f27337b49222b
Marcos Frony <mfrony@yahoo.com>
2024-03-24 22:26:52 -0700

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,133 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="mx-autow-full">
+      <header className="flex items-center justify-between flex-wrap bg-amber-50 pb-4 mx-20 mr-20 ">
+        {/* Logo */}
+        <div className="flex-shrink-0 ml-6 mr-6 md:pl-2 md:pr-6 md:pt-12">
+          {/*<TribeLogo/>*/}
+          <h1 className={'font-bold text-4xl text-stone-500 pl-6'}>Kawori - かおり</h1>
+        </div>
+      </header>
+
+      {/* First panel */}
+      <div className={'pb-0 px-20 h-full'}>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-0 gap-y-2 border-b-indigo-950 border-t-2">
+          <div className="bg-amber-50 pb-16 pl-8 h-full px-16 pt-10">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={'/img/valentines-2021.jpg'}
+              alt={'Golden Gate Bridge viewed from Chrissy Fields'}
+              width={4032}
+              height={2268}
+              className={'h-64 object-cover'}
             />
-          </a>
+          </div>
+          <div className="bg-amber-50 pl-8 pt-20 px-16 font-bold">
+            <h1 className={`mb-3 text-5xl pt-2`}>Kawori's</h1>
+            <h1 className={`mb-3 text-4xl`}>Live Your Potential</h1>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      {/* Second panel */}
+      <div className={'mx-20 px-16 h-full bg-amber-200 py-0'}>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-2 pb-16">
+          <div className={'col-span-3 md:pb-16'}>
+            <h1 className={'border-b-indigo-950 border-b-2 text-4xl pt-10 pb-3'}>＝ダイエットカウンセリング＝</h1>
+          </div>
+          <div className="h-full col-span-2">
+            <p className={'pt-1 my-3'}>体重の増減に一喜一憂するのに、疲れたあなたに。</p>
+            <p className={'pt-1 my-3'}>「何をやってもなかなか体重が落ちない」、「ダイエットとリバウンドを繰り返している」、「若い時にうまくいった方法が効かなくなった」、「自分にあった方法がわからない」、「最後のダイエットにしたい」。 そんなお悩みを抱える方々のご要望にお応えして、オンラインでのマンツーマンのダイエットサポートを行なっております。</p>
+            <p className={'pt-1 my-3'}>「ダイエット伴奏者、佐藤かをり」が、東洋医学の考え方に基づいて、あなたの体質を分析。あなたの体質やライフスタイルに合った「あなただけの痩せ方」を見つけるお手伝いをします。</p>
+          </div>
+          <div className={'p-10'}>
+            <Image
+              src={'/img/crissy-fields.jpg'}
+              alt={'Golden Gate Bridge viewed from Chrissy Fields'}
+              width={4032}
+              height={2268}
+              className={'h-80 w-full'}
+            />
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Third panel */}
+      <div className={'mx-20 px-16 h-full bg-amber-50 py-0'}>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-2 pb-16">
+          <div className={'col-span-3 md:pb-16'}>
+            <h1 className={' border-b-indigo-950 border-b-2 text-4xl pt-10 pb-3'}>What we can do for you</h1>
+          </div>
+          <div className="h-full">
+            <h2 className={'text-2xl'}>01</h2>
+            <Image
+              src={'/img/dinner.jpg'}
+              alt={'Golden Gate Bridge viewed from Chrissy Fields'}
+              width={4032}
+              height={2268}
+              className={'h-80 p-2 pl-0'}
+            />
+          </div>
+          <div className="h-full">
+            <h2 className={'text-2xl'}>02</h2>
+            <Image
+              src={'/img/dessert.jpg'}
+              alt={'Golden Gate Bridge viewed from Chrissy Fields'}
+              width={4032}
+              height={2268}
+              className={'h-80 p-2 pl-0 object-contain'}
+            />
+          </div>
+          <div className="h-full">
+            <h2 className={'text-2xl'}>03</h2>
+            <Image
+              src={'/img/green.jpg'}
+              alt={'Green street'}
+              width={4032}
+              height={2268}
+              className={'h-80 p-2 pl-0 object-cover'}
+            />
+          </div>
+        </div>
+      </div>
+      {/* Fourth panel */}
+      <div className={'mx-20 px-16 h-full bg-amber-200 py-0'}>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-2 pb-16">
+          <div className={'col-span-3 md:pb-16'}>
+            <h1 className={' border-b-indigo-950 border-b-2 text-4xl pt-10 pb-3'}>ソーシャルメディア＆お問い合わせ</h1>
+          </div>
+          <div className="h-full col-span-2">
+            <Image
+              src={'/img/mushrooms.jpg'}
+              alt={'Green street'}
+              width={4032}
+              height={2268}
+              className={'h-80 p-2 pl-0 object-cover'}
+            />
+          </div>
+          <div className={'p-10'}>
+            <Link href={'https://www.instagram.com/kaworislyp/'}>
+              <Image
+                src={'/img/instagram_social_media_icon.png'}
+                alt={'Green street'}
+                width={60}
+                height={60}
+                className={'h-14 p-2 pl-0 object-cover'}
+              />
+            </Link>
+            <Link href={'https://liff.line.me/1645278921-kWRPP32q/?accountId=330kpivn'}>
+              <Image
+                src={'/img/line_social_media_icon.png'}
+                alt={'Green street'}
+                width={60}
+                height={60}
+                className={'h-14 p-2 pl-0 object-cover'}
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
